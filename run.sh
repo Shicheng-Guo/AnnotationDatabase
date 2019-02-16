e140c6f95ac9b2c9e97636593bfd5a063b23299d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-15 19:08:21 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-
 wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz 
 awk '{print $3,$5,$6,$4,$2,$13}' OFS="\t" refGene.txt | bedtools sort  > refGene.hg19.bed
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/refGene.txt.gz 
+awk '{print $3,$5,$6,$4,$2,$13}' OFS="\t" refGene.txt | bedtools sort  > refGene.hg38.bed
