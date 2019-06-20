8b6055a0a5fbdf6fe64bbe4f553c11d8c6222902
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-19 19:35:42 -0500

    Create fixdbSNP152VCF.pl

---
+++ dbSNP/fixdbSNP152VCF.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+$ARGV[1] or die "use fixdbSNP152VCF.pl <VCF> <FIX_VCF>\n";
+my $orig_vcf = shift @ARGV;
+my $new_vcf = shift @ARGV;
+my (%seq,$chr, $snp,$pos, $ref, $obs);
+open (my $vh, "<", $orig_vcf) or die "cannot read $orig_vcf\n";
+open (my $oh, ">", $new_vcf) or die "cannot write $new_vcf\n";
+while (<$vh>) {
+    if (/^#/) {
+         print $oh $_;
+    } else {
+        chomp;
+        my @var = split (/\t/, $_);
+        $var[0]= $1 if ($var[0] =~ /0+(\d+)/);
+        print $oh join "\t", @var;
+        print $oh "\n";
+    }
+}
+close $vh;
+close $oh;
+
+
