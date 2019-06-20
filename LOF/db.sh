d4a5ff405c9dcfc8aae1c60dcc167e2533fcd655
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-20 00:34:46 -0500

    Update db.sh

--- LOF/db.sh
+++ LOF/db.sh
@@ -2,3 +2,7 @@
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/integrated_sv_map/ALL.wgs.mergedSV.v8.20130502.svs.genotypes.vcf.gz
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/integrated_sv_map/ALL.wgs.mergedSV.v8.20130502.svs.genotypes.vcf.gz.tbi
 
+# dbNSFP4.0a
+wget ftp://dbnsfp.softgenetics.com/dbNSFP4.0a.zip
+
+
