dff9bdb3b899704c65edd91d2545288a6ab02003
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-20 17:14:37 -0500

    Update aloft.pbs.sh

--- LOF/aloft.pbs.sh
+++ LOF/aloft.pbs.sh
@@ -18,3 +18,46 @@ echo ~/hpc/tools/aloft/aloft-annotate/aloft --vcf gnomad.exomes.r2.1.sites.chr$i
 qsub $i.job
 done
 
+#### Summary the Reslt
+
+for i in `ls *.lof`
+do
+awk '{print $1,$2,$3,$4,$5}' $i  |sort -u | wc -l
+done
+
+for i in `ls *.splice`
+do
+awk '{print $1,$2,$3,$4,$5}' $i  |sort -u | wc -l
+done
+
+##### Gnomad Result
+
+	LOF	SPLICE
+gnomad.exomes.r2.1.sites.chr10.dq.rec.vcf.gz.vat.aloft.lof	17960	4118
+gnomad.exomes.r2.1.sites.chr11.dq.rec.vcf.gz.vat.aloft.lof	30685	5993
+gnomad.exomes.r2.1.sites.chr12.dq.rec.vcf.gz.vat.aloft.lof	24888	5887
+gnomad.exomes.r2.1.sites.chr13.dq.rec.vcf.gz.vat.aloft.lof	8010	1754
+gnomad.exomes.r2.1.sites.chr14.dq.rec.vcf.gz.vat.aloft.lof	15019	2980
+gnomad.exomes.r2.1.sites.chr15.dq.rec.vcf.gz.vat.aloft.lof	17133	3834
+gnomad.exomes.r2.1.sites.chr16.dq.rec.vcf.gz.vat.aloft.lof	21989	5056
+gnomad.exomes.r2.1.sites.chr17.dq.rec.vcf.gz.vat.aloft.lof	28625	6212
+gnomad.exomes.r2.1.sites.chr18.dq.rec.vcf.gz.vat.aloft.lof	6958	1548
+gnomad.exomes.r2.1.sites.chr19.dq.rec.vcf.gz.vat.aloft.lof	39582	6419
+gnomad.exomes.r2.1.sites.chr20.dq.rec.vcf.gz.vat.aloft.lof	11101	2483
+gnomad.exomes.r2.1.sites.chr21.dq.rec.vcf.gz.vat.aloft.lof	5328	1087
+gnomad.exomes.r2.1.sites.chr22.dq.rec.vcf.gz.vat.aloft.lof	10298	2359
+gnomad.exomes.r2.1.sites.chr2.dq.rec.vcf.gz.vat.aloft.lof	32791	7885
+gnomad.exomes.r2.1.sites.chr3.dq.rec.vcf.gz.vat.aloft.lof	28133	6281
+gnomad.exomes.r2.1.sites.chr4.dq.rec.vcf.gz.vat.aloft.lof	19306	4304
+gnomad.exomes.r2.1.sites.chr5.dq.rec.vcf.gz.vat.aloft.lof	21271	4208
+gnomad.exomes.r2.1.sites.chr6.dq.rec.vcf.gz.vat.aloft.lof	24908	5171
+gnomad.exomes.r2.1.sites.chr7.dq.rec.vcf.gz.vat.aloft.lof	22953	4882
+gnomad.exomes.r2.1.sites.chr8.dq.rec.vcf.gz.vat.aloft.lof	16116	3677
+gnomad.exomes.r2.1.sites.chr9.dq.rec.vcf.gz.vat.aloft.lof	19129	4031
+gnomad.exomes.r2.1.sites.chrX.dq.rec.vcf.gz.vat.aloft.lof	7329	1491
+gnomad.exomes.r2.1.sites.chrY.dq.rec.vcf.gz.vat.aloft.lof	150	39
+	                                                        429662	91699
+
+
+      
+
