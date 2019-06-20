39b11e3d5aca35ef870a108aadc8d43ec81c35eb
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-20 00:25:51 -0500

    Create db.sh

---
+++ LOF/db.sh
@@ -0,0 +1,4 @@
+# 1000 genome phase3
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/integrated_sv_map/ALL.wgs.mergedSV.v8.20130502.svs.genotypes.vcf.gz
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/integrated_sv_map/ALL.wgs.mergedSV.v8.20130502.svs.genotypes.vcf.gz.tbi
+
