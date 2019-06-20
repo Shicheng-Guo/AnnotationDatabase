c9e8d06b6ea21eb86e91f9743e466a2362e490fe
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-20 00:48:20 -0500

    Create annovar.pbs.sh

---
+++ LOF/annovar.pbs.sh
@@ -0,0 +1,15 @@
+mkdir annovar
+mkdir temp
+for i in {1..22} 
+do
+echo \#PBS -N $i  > chr$i.job
+echo \#PBS -l nodes=1:ppn=8 >> chr$i.job
+echo \#PBS -M Guo.shicheng\@marshfieldresearch.org >> chr$i.job
+echo \#PBS -m abe  >> chr$i.job
+echo \#PBS -o $(pwd)/temp/ >>chr$i.job
+echo \#PBS -e $(pwd)/temp/ >>chr$i.job
+echo cd $(pwd) >> chr$i.job
+echo convert2annovar.pl -format vcf4 -allsample -withfreq gnomad.exomes.r2.1.sites.chr$i.rec.vcf.gz  \> ./annovar/gnomad.exomes.r2.1.sites.chr$i.rec.vcf.avinput >> chr$i.job
+echo table_annovar.pl ./annovar/gnomad.exomes.r2.1.sites.chr$i.rec.vcf.avinput /gpfs/home/guosa/hpc/tools/annovar/humandb/ --thread 4 -buildver hg19 --csvout -out ./annovar/chr$i -remove -protocol refGene,dbnsfp33a,gwasCatalog,wgRna,targetScanS,tfbsConsSites -operation gx,f,r,r,r,r -nastring . -otherinfo -polish -xref /gpfs/home/guosa/hpc/tools/annovar/humandb/gene_fullxref.txt >> chr$i.job
+qsub chr$i.job
+done
