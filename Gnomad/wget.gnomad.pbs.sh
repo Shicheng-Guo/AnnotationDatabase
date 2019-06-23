fd0acdcf1d36b4c9da603304792e33335f0528c8
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-22 19:05:39 -0500

    Update wget.gnomad.pbs.sh

--- Gnomad/wget.gnomad.pbs.sh
+++ Gnomad/wget.gnomad.pbs.sh
@@ -3,7 +3,7 @@ mkdir temp
 for i in {1..22} X Y
 do
 echo \#PBS -N $i  > $i.job
-echo \#PBS -l nodes=1:ppn=16 >> $i.job
+echo \#PBS -l nodes=1:ppn=1 >> $i.job
 echo \#PBS -M Guo.shicheng\@marshfieldresearch.org >> $i.job
 echo \#PBS -m abe  >> $i.job
 echo \#PBS -o $(pwd)/temp/ >>$i.job
