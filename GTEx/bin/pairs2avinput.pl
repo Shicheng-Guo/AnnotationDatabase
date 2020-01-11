a925b65fd06e3b931c26c5172ec61f261a469187
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-11 01:09:25 -0600

    Update pairs2avinput.pl

--- GTEx/bin/pairs2avinput.pl
+++ GTEx/bin/pairs2avinput.pl
@@ -1,1 +1,19 @@
+# /home/guosa/hpc/project/m6A/GTEx_Analysis_v8_eQTL
 
+use strict;
+use Cwd;
+
+open F1,"";
+while(<F1>){
+
+}
+
+
+
+my $file=shift @ARGV;
+open F,$file;
+while(<F>){
+my @line=split /\s+/;
+my(undef,$chr,$pos,$ref,$alt,$hg)=split /chr|_/,$line[0];
+print "$chr\t$pos\n";
+}
